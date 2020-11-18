ada36102dbacd52d814c4f766a7dc7c1582e034d
Sonia-SV <sonia.sanzvivas@gmail.com>
2020-11-18 23:16:59 +0100

    Arreglar error de ordenación

--- src/services/spotify.js
+++ src/services/spotify.js
@@ -21,7 +21,8 @@ export const getTokenFromUrl = () => window.location.hash
     const parts = item.split('=');
     initial[parts[0]] = decodeURIComponent(parts[1]);
     return initial;
-  });
+  },
+  {});
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes
   .join('%20')}&response_type=token&show_dialog=true`;
